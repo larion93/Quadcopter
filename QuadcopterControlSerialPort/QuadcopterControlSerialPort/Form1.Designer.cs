0fe23262e0ba9864f58e391df4241712cf95a4b6
larion666 <larion93@rambler.ru>
2014-12-22 23:01:24 +0200

    no message

---
+++ QuadcopterControlSerialPort/QuadcopterControlSerialPort/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace QuadcopterControlSerialPort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.PortName = "COM3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Pitch angle";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Roll angle";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(78, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "test";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(78, 56);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "test";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 82);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Throttle";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(78, 82);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(24, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "test";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 145);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ QuadcopterControlSerialPort/QuadcopterControlSerialPort/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace QuadcopterControlSerialPort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.Open();
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+        }
+        void SendASCIItoSerialPort(int ASCII)
+        {
+            byte[] buffer = new byte[] { Convert.ToByte(ASCII) };
+            serialPort1.Write(buffer,0,1);
+        }
+         protected override void OnKeyDown(KeyEventArgs e)
+        {
+             base.OnKeyDown(e);
+             SendASCIItoSerialPort(e.KeyValue);
+        }
+        delegate void SetTextCallback(string text);
+        private void SetText(string text)
+        {
+
+            if (this.label1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.label1.Text = text;
+            }
+        }
+        private void SetText1(string text)
+        {
+
+            if (this.label5.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText1);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.label5.Text = text;
+            }
+        }
+        private void SetText2(string text)
+        {
+
+            if (this.label6.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText2);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.label6.Text = text;
+            }
+        }
+        private void SetText3(string text)
+        {
+
+            if (this.label9.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText3);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.label9.Text = text;
+            }
+        }
+        private void DataReceivedHandler( object sender, SerialDataReceivedEventArgs e)
+        {
+            string message = serialPort1.ReadLine();
+            SetText(message.ToString());
+            string[] parameters= new string[9];
+            parameters = message.Split(',');
+            if(parameters.Length>8)
+            {
+                SetText1(parameters[1]);
+                SetText2(parameters[4]);
+                //SetText3(parameters[2]);
+                SetText3(parameters[6]);
+            }
+        }
+
+    }
+}
